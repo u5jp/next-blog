96cebf7d2455f9b6de81eb86502a840f7a444bfa
yugo_i <u15jpn@gmail.com>
2020-12-09 02:28:52 +0900

    fetch data init

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,9 +1,9 @@
-const CardItem = ({ className }) => {
+const CardItem = ({ title, className }) => {
   return (
     <div className={className}>
       <div className="card">
         <img className="card_img" src={"https://via.placeholder.com/150"} />
-        <p className="card_title">タイトル</p>
+        <p className="card_title">{title}</p>
         <p className="card_text">サブタイトル</p>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,33 @@ import PageLayout from "components/PageLayout";
 import Intro from "components/Intro";
 import CardItem from "components/CardItem";
 
-export default function Home() {
+import { getAllPosts } from "lib/index";
+
+export default function Home({ blogs }) {
+  console.log(blogs);
   return (
     <PageLayout>
       <Intro />
       <hr />
+      {JSON.stringify(blogs)}
       <div className="container">
-        <CardItem className="container_item" />
+        {blogs.map((blog) => (
+          <CardItem
+            className="container_item"
+            key={blog.fields.slug}
+            title={blog.fields.title}
+          />
+        ))}
       </div>
     </PageLayout>
   );
+}
+
+export async function getStaticProps() {
+  const blogs = await getAllPosts();
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
