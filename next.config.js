6b4fe4625402a31ef184e05abe686c210061ab3f
yugo_i <u15jpn@gmail.com>
2020-12-17 20:20:15 +0900

    add production sorcemaps

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  options: {
+    sourcemaps: "production", // possible values can be production, development, or none
+  },
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_API_KEY: process.env.CONTENTFUL_API_KEY,
