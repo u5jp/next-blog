502516db7ceeb944e35901bf88dfe9b1b5123982
yugo_i <u15jpn@gmail.com>
2020-12-19 00:29:42 +0900

    delete console.log

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import React, { FC } from 'react';
 
 const Navbar: FC = () => {
-  console.log("navbar描画")
   return (
     <nav className="ly_nav">
       <Link href="/">
