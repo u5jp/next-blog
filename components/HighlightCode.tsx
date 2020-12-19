1199d70dd2e26ef6bf783f200963594e346bcb07
yugo_i <u15jpn@gmail.com>
2020-12-19 20:22:08 +0900

    improvement hljs

--- components/HighlightCode.tsx
+++ components/HighlightCode.tsx
@@ -1,12 +1,17 @@
-import highlight from 'highlight.js';
+import hljs from 'highlight.js/lib/core';
 import { createRef, FC, useEffect } from 'react';
 import { findDOMNode } from 'react-dom';
+
+hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
+
 
 const HighlightCode:FC = ({ children }) => {
   const code = createRef<HTMLElement>();
 
   useEffect(() => {
-    highlight.highlightBlock(findDOMNode(code.current));
+    hljs.highlightBlock(findDOMNode(code.current));
   });
 
   return (
